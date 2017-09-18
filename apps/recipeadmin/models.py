e7508ab5cda4a1e85d1f8b619fe0c658ee4fdcb7
Mounika <mounika.tunga9@gmail.com>
2017-09-18 10:44:18 -0700

    confirm

--- apps/recipeadmin/models.py
+++ apps/recipeadmin/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class UserManager(models.Manager):
+    def login(self, postData):
+            print "in def1"
+            results = {'status': True, 'errors': [],'user':None}
+
+
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=38)
+    last_name = models.CharField(max_length=38)
+    emailid = models.CharField(max_length=78)
+    password = models.CharField(max_length=38)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    objects = UserManager()
